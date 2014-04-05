a1f06b041e8a9e32b5ef7f3e32e3c5e9e009b72a
Andrey Yuferov <asjuferov@gmail.com>
2014-04-05 17:10:30 +0400

    DotEnv for secret tokens.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,9 @@
-# Be sure to restart your server when you modify this file.
+Cadenza::Application.config.secret_key_base = ENV['APP_SECRET_KEY_BASE']
 
-# Your secret key is used for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
+VkontakteApi.app_id       = ENV["VK_APP_ID"]
+VkontakteApi.app_secret   = ENV["VK_APP_SECRET"]
+VkontakteApi.redirect_uri = ENV["VK_REDIRECT_URI"]
 
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Cadenza::Application.config.secret_key_base = '9a0f13a6774b2cc4fb35e4019b2fcb984d5c20f2f95ffe59fbf48e7bec3f8a78baf716f3e592bd6aa8f19f11990a0ecf98ec11292caa016cfef40d9253bc6586'
+Echowrap.api_key       = ENV["ECHONEST_API_KEY"]
+Echowrap.consumer_key  = ENV["ECHONEST_CONSUMER_KEY"]
+Echowrap.shared_secret = ENV["ECHONEST_SHARED_SECRET"]
